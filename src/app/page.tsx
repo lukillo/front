b4350b9f7ee226be29f80a30d96a9ad271b9f52b
lucas llopis <lucasllopis@ip-192-168-0-253.us-west-2.compute.internal>
2024-12-04 16:40:36 -0300

    Add central card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 'use client'
 
-import { Header } from '@/components/header'
-
+import { Header } from '@/components/Header'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default function AdminPage() {
   return (
     <div className="min-h-screen bg-secondary-light">
       <Header />
       <main className="container mx-auto mt-8 p-4">
+        <Card className="w-full max-w-2xl mx-auto">
+            <CardHeader>
+              <CardTitle className="text-2xl font-bold text-center">Email Campaign Management</CardTitle>
+            </CardHeader>
+        </Card>
       </main>
     </div>
   )
